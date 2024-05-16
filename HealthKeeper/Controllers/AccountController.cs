0610533b90da50a45c0f9cdea1360509b5bc6349
Lukas <me@lhertkorn.de>
2024-05-16 22:12:56 +0200

    feat: routing is now working properly.

---
+++ HealthKeeper/Controllers/AccountController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace HealthKeeper.Controllers;
+
+[Route("[controller]")]
+public class AccountController : Controller
+{
+
+    private DatabaseContext _ctx;
+
+    public AccountController(DatabaseContext ctx)
+    {
+        _ctx = ctx;
+    }
+
+    [HttpGet("Login")]
+    public ActionResult Login()
+    {
+        return View();
+    }
+}

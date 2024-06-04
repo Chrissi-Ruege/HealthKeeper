45e5e38f42491552f448514b01007691005605da
Lukas <me@lhertkorn.de>
2024-06-04 09:40:55 +0200

    fix: pipeline

--- HealthKeeper/Models/Database/StatisticEntry.cs
+++ HealthKeeper/Models/Database/StatisticEntry.cs
@@ -9,10 +9,5 @@ public class StatisticEntry
     public DateTime Timestamp { get; set; }
     public double Weight { get; set; }
     public double Height { get; set; }
-
-<<<<<<< HEAD
-    public virtual User? User { get; set; }
-=======
-    public virtual IdentityUser User { get; set; }
->>>>>>> 5671f99f3ae91b509feee3f29c2b15d2072e6e49
+    public virtual IdentityUser? User { get; set; }
 }
